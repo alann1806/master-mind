9af279ade2ced27cf572dbf43150bf6ee0308b88
alann1806 <allubet@edenschool.com>
2019-10-24 17:31:17 +0200

    Updates

---
+++ precache-manifest.87ffba428ea3b7f623e9e5feca83fdb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c631fc841e4bc025b8f6200b55e4b2e7",
+    "url": "://alann1806.github.io/master-mind/index.html"
+  },
+  {
+    "revision": "296f309c61688207850b",
+    "url": "://alann1806.github.io/master-mind/static/css/main.6b104ed3.chunk.css"
+  },
+  {
+    "revision": "2e74cbd12b0fcd3d815f",
+    "url": "://alann1806.github.io/master-mind/static/js/2.236322fe.chunk.js"
+  },
+  {
+    "revision": "296f309c61688207850b",
+    "url": "://alann1806.github.io/master-mind/static/js/main.2227b40a.chunk.js"
+  },
+  {
+    "revision": "ca40627c2bb8091d1459",
+    "url": "://alann1806.github.io/master-mind/static/js/runtime-main.fdf3498e.js"
+  }
+]);
